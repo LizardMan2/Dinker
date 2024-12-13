e0929e7121ea4ef8958d4dd4929da05846ef18ac
LizardMan2 <rudylkirby@gmail.com>
2024-12-13 15:16:47 -0500

    Finished the in editor level editor

--- Objects/New Level Creation/data.gd
+++ Objects/New Level Creation/data.gd
@@ -30,33 +30,41 @@ enum col{Purple, Blue, Red, Yellow}
 
 @export var color: col
 
-
+var previous = [Vector2i.ZERO, 0, 0, 1.0, 1, false, 0, 0]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
-		position = (Vector2(pos.x, pos.y) * 15) + Vector2(7.5, 7.5)
-		match type:
-			0: #"Bumper"
-				var nums = Vector2(startDirection * 15, color * 15)
-				texture.region = Rect2(nums.x, nums.y, 15, 15)
-			1: #"Door"
-				var nums = Vector2(startDirection * 15 + 60, color * 15)
-				texture.region = Rect2(nums.x, nums.y, 15, 15)
-			2: #"Player"
-				texture.region = Rect2(0, 60, 15, 15)
-			3: #"Btn"
-				var nums = Vector2(color * 15, 90)
-				texture.region = Rect2(nums.x, nums.y, 15, 15)
-			4: #"FourPoint"
-				texture.region = Rect2(0, 105, 15, 15)
-			5: #"hitAll"
-				texture.region = Rect2(15, 105, 15, 15)
-			6: #"Lock"
-				texture.region = Rect2(30, 105, 15, 15)
-			7: #"Key"
-				texture.region = Rect2(45, 105, 15, 15)
-			8: #"Crotator"
-				var nums = Vector2(startDirection * 15 + 60, 105)
-				texture.region = Rect2(nums.x, nums.y, 15, 15)
+		var current = [pos, startDirection, endDirection, speed, length, inverted, type, color]
+		if previous != current:
+			previous = current
+			print("Changed")
+			position = (Vector2(pos.x, pos.y) * 15) + Vector2(7.5, 7.5)
+			match type:
+				0: #"Bumper"
+					var nums = Vector2(startDirection * 15, color * 15)
+					texture.region = Rect2(nums.x, nums.y, 15, 15)
+				1: #"Door"
+					if not color in [1, 2]:
+						color = 1
+					var nums = Vector2(startDirection * 15 + 60, color * 15 - 15)
+					texture.region = Rect2(nums.x, nums.y, 15, 15)
+				2: #"Player"
+					texture.region = Rect2(0, 60, 15, 15)
+				3: #"Btn"
+					if not color in [1, 2]:
+						color = 1
+					var nums = Vector2(color * 15 - 15, 90)
+					texture.region = Rect2(nums.x, nums.y, 15, 15)
+				4: #"FourPoint"
+					texture.region = Rect2(0, 105, 15, 15)
+				5: #"hitAll"
+					texture.region = Rect2(15, 105, 15, 15)
+				6: #"Lock"
+					texture.region = Rect2(30, 105, 15, 15)
+				7: #"Key"
+					texture.region = Rect2(45, 105, 15, 15)
+				8: #"Crotator"
+					var nums = Vector2(startDirection * 15 + 60, 105)
+					texture.region = Rect2(nums.x, nums.y, 15, 15)
 		

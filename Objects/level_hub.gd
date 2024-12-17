45ba9c969f8dd62a58689c7510ee98d2025f2868
LizardMan2 <rudylkirby@gmail.com>
2024-12-17 14:54:43 -0500

    Red 1 and fixed thing

--- Objects/level_hub.gd
+++ Objects/level_hub.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $Tree.get_selected():
+	if $Tree.get_selected() and $Tree.get_selected().get_metadata(0):
 		$Confirm.visible = true
 		$Confirm/Label.text = str($Tree.get_selected().get_metadata(0))
 	else:
